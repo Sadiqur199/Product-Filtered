82650dea61b181304a5a67ccce7434d3b11a5885
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-10-05 04:11:18 +0600

    implement data

--- src/Component/ProductFilter/ProductFilter.jsx
+++ src/Component/ProductFilter/ProductFilter.jsx
@@ -11,7 +11,7 @@ const ProductFilter = () => {
   const ProductsFilter = () =>{
     const [selectedCategory, setSelectedCategory] = useState("");
     const [maxPrice , setMaxPrice] = useState(1000);
-    const [filteredProducts, setFilteredProcuts] = useState(ProductsData)
+    const [filteredProducts, setFilteredProducts] = useState(ProductsData)
 
     const handleCategoryChange = (e) =>{
       setSelectedCategory(e.target.value)
@@ -19,11 +19,50 @@ const ProductFilter = () => {
 
     const handlePriceChange = (e) =>{
       setMaxPrice(e.target.value)
+    };
+
+    const applyFilter = () =>{
+      let updateProducts = ProductsData;
+      if(selectedCategory !== "") {
+        updateProducts = updateProducts.filter(
+          (product) => product.category === selectedCategory
+        )
+      }
+      updateProducts = updateProducts.filter(
+        (product) => product.price <= maxPrice 
+      )
+      setFilteredProducts(updateProducts)
     }
   }
   return (
     <div>
-      
+      <div className='p-5'>
+        <h1 className='text-xl font-bold'>Filtered Products</h1>
+        <label className='block mt-5'> <span>Category:</span> 
+        <select value={selectedCategory} onChange={handleCategoryChange} className='block w-full mt-2'/>
+        <option value="">All</option>
+        <option value='Electronics'>Electronics</option>
+        <option value='Books'>Books</option>
+        <option value='Clothing'>Clothing</option>
+        </label>
+        <label className='block mt-5'>
+ <span> Max Price : ${maxPrice}</span>
+ <input 
+ type='range'
+ min='0'
+ max="1000"
+ value={maxPrice}
+ onChange={handlePriceChange}
+ className='block w-full mt-2'
+ />
+        </label>
+        <button onClick={applyFilter} className='mt-5 bg-blue-500 text-white p-3 rounded'>Filter Now</button>
+      </div>
+
+<div >
+
+</div>
+
     </div>
   );
 };
